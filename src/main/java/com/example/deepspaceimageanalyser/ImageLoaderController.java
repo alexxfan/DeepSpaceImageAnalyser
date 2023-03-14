9bdcc0fdbed86f276735bdf9e19e5da15e420d7f
Alex Fan <20077347@mail.wit.ie>
2023-03-14 13:25:59 +0000

    few changes, added colouring to pixels

--- src/main/java/com/example/deepspaceimageanalyser/ImageLoaderController.java
+++ src/main/java/com/example/deepspaceimageanalyser/ImageLoaderController.java
@@ -15,10 +15,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class ImageLoaderController implements Initializable {
     FileChooser fileChooser = new FileChooser(); // file chooser
@@ -27,6 +24,9 @@ public class ImageLoaderController implements Initializable {
     private int[] imageArray;
     private int width;
     private int height;
+    private Color color;
+
+    Hashtable<Integer, ArrayList<Integer>> hashTable;
 
     @FXML
     public ImageView regularImage, blackAndWhiteImageView, redImage, greenImage, blueImage, adjustableImage;
@@ -64,11 +64,12 @@ public class ImageLoaderController implements Initializable {
     public WritableImage blackAndWhiteImg(ActionEvent actionEvent) {
 
         Image image = regularImage.getImage(); //get the chosen image from file explorer (regular imageview)
-        blackAndWhiteImageView.setImage(image); //set grayscale image to this
+        blackAndWhiteImageView.setImage(image); //set black and white image to this
         PixelReader pixelReader = image.getPixelReader(); //initialize the pixel reader for the original image
 
-        int width = (int) image.getWidth(); //get width of image
-        int height = (int) image.getHeight(); //get height of image
+        width = (int) image.getWidth(); //get width of image
+        height = (int) image.getHeight(); //get height of image
+        imageArray = new int[(height * width)];
 
         WritableImage blackAndWhiteImage = new WritableImage(width, height); //create a new WritableImage object with the same dimensions as the original image
         PixelWriter pixelWriter = blackAndWhiteImage.getPixelWriter();
@@ -84,7 +85,6 @@ public class ImageLoaderController implements Initializable {
 
                 //calculate the luminance of the current pixel
                 double luminance = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
-//                double luminanceThreshold = 0.5;
                 double luminanceThreshold = Double.parseDouble(thresh.getText());
 
                 //determine whether the pixel should be white or black based on the luminance threshold
@@ -101,34 +101,70 @@ public class ImageLoaderController implements Initializable {
     }
 
 
-    public void joinPixelsInArray(){ //joins adjacent pixels in an image array, using the union method
-        for (int i = 0; i < imageArray.length; i++){  //loop through each pixel in the image array
-            if (imageArray[i] >= 0) {  //check if the pixel is not already part of a group
-                if ((i + 1) < imageArray.length && imageArray[i + 1] >= 0 && (i + 1) % width != 0){ //checks if the pixel to the right is also part of a group and not at the edge of the image
-                    union(imageArray, i, i + 1); //join the pixels using the union method
-                }
-                if ((i + width) < imageArray.length && imageArray[i + width] >= 0){ //checks if the pixel below is also part of a group
-                    union(imageArray, i, i + width); //join the pixels using the union method
-                }
-            }
-        }
-    }
 
     public int find(int[] a, int id) {
         return a[id] == id ? id : (a[id] = find(a, a[id])); //finding the root of id
-    }
+    } // keeps recursively calling itself until the root is found
 
     public void union(int[] a, int p, int q) { //merges two groups by connecting the root element of one group to the root element of another group
         a[find(a, q)] = find(a, p); //set the root of the group containing q to the root of the group containing p
     }
 
+    public void unionPixels(int a, int b) {
+        if(find(imageArray, a) < find(imageArray, b))
+            union(imageArray, a, b);
+        else union(imageArray, b, a);
+    }
 
-        public void exit(ActionEvent actionEvent){
-        //exits app
-        Platform.exit();
+//    public boolean pixelsAreWhite(int i){
+//        return imageArray[i] != -1;
+//    }
+
+    public WritableImage randomColorImg(ActionEvent actionEvent) {
+
+        Image image = blackAndWhiteImageView.getImage(); //get the chosen image from the black image view
+        PixelReader pixelReader = image.getPixelReader(); //initialize the pixel reader for the original image
+
+        width = (int) image.getWidth(); //get width of image
+        height = (int) image.getHeight(); //get height of image
+        imageArray = new int[(height * width)];
+
+        WritableImage randomColorImage = new WritableImage(width, height); //create a new WritableImage object with the same dimensions as the original image
+        PixelWriter pixelWriter = randomColorImage.getPixelWriter();
+
+        //loop through each pixel of the original image
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+
+                //get the red, green, and blue values of the current pixel
+                double red = pixelReader.getColor(x, y).getRed();
+                double green = pixelReader.getColor(x, y).getGreen();
+                double blue = pixelReader.getColor(x, y).getBlue();
+
+                //calculate the luminance of the current pixel
+                double luminance = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
+                double luminanceThreshold = Double.parseDouble(thresh.getText());
+
+                //determine whether the pixel should be coloured or black based on the luminance threshold
+                if (luminance >= luminanceThreshold) {
+                    //generate a random color for the current pixel
+                    Color randomColor = Color.rgb((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+                    randomColorImage.getPixelWriter().setColor(x, y, randomColor);
+                } else {
+                    randomColorImage.getPixelWriter().setColor(x, y, Color.BLACK);
+                }
+
+            }
+        }
+        blackAndWhiteImageView.setImage(randomColorImage); //set the grayImageView to display the new grayscale image
+        return randomColorImage; //return the new colored image
     }
 
 
+    public void exit(ActionEvent actionEvent){
+        //exits app
+        Platform.exit();
+    }
 
 
     @Override
