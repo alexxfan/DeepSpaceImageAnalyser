d197fdeeaa091fbc6d90adf2b360bd07605e53e0
Alex Fan <20077347@mail.wit.ie>
2023-03-29 14:55:01 +0100

    got random colouring

--- src/main/java/com/example/deepspaceimageanalyser/ImageLoaderController.java
+++ src/main/java/com/example/deepspaceimageanalyser/ImageLoaderController.java
@@ -29,7 +29,7 @@ public class ImageLoaderController implements Initializable {
     private Color white, black;
     private WritableImage newBlackAndWhiteImage;
 
-    Hashtable<Integer, ArrayList<Integer>> hashTable;
+    HashMap<Integer, ArrayList<Integer>> hashMap;
 
     @FXML
     public ImageView regularImage, blackAndWhiteImageView, redImage, greenImage, blueImage, adjustableImage;
@@ -76,6 +76,7 @@ public class ImageLoaderController implements Initializable {
 //            System.out.println("Error in image");
 //        }
         pixelArray = new int[(height * width)];
+        hashMap = new HashMap<>();
 //        Arrays.fill(pixelArray, -1); // initialize all pixels to -1
 
         WritableImage blackAndWhiteImage = new WritableImage(width, height); //create a new WritableImage object with the same dimensions as the original image
@@ -128,15 +129,34 @@ public class ImageLoaderController implements Initializable {
         blackAndWhiteImageView.setImage(blackAndWhiteImage); //set the blackAndWhiteImageView to display the new image
         newBlackAndWhiteImage = blackAndWhiteImage; //return the new image
 
-        displayPixelArray();
+        for(int row = 0; row < height; row++){
+            for(int column = 0; column < width; column++){
+                int colour = pixelArray[((row*width)+column)];
+
+                if(colour >= 0){
+                    int index = (row * width) + column;
+                    int root = find(pixelArray, index);
+
+                    if(!hashMap.containsKey(root)){
+                        hashMap.put(root, new ArrayList<>());
+                    }
+                    hashMap.get(root).add(index);
+                }
+            }
+        }
+
+        int largestRoot = Collections.max(hashMap.keySet(),(a, b)-> hashMap.get(a).size() - hashMap.get(b).size());
+        List list = hashMap.get(largestRoot);
+
+//        displayPixelArray();
         return newBlackAndWhiteImage;
 
     }
 
-    public void displayPixelArray() {
-        for(int i=0;i<pixelArray.length;i++)
-            System.out.print(find(pixelArray,i) + ((i+1)%width==0 ? "\n" : " "));
-    }
+//    public void displayPixelArray() {
+//        for(int i=0;i<pixelArray.length;i++)
+//            System.out.print(find(pixelArray,i) + ((i+1)%width==0 ? "\n" : " "));
+//    }
 
     public void unionPixels(int a, int b) {
         if(find(pixelArray, a) < find(pixelArray, b))
@@ -154,61 +174,99 @@ public class ImageLoaderController implements Initializable {
         a[find(a, q)] = find(a, p); //set the root of the group containing q to the root of the group containing p
     }
 
-//    public void unionPixels(int a, int b) {
-//        int rootA = find(pixelArray, a);
-//        int rootB = find(pixelArray, b);
-//        if (rootA != rootB) {
-//            union(pixelArray, rootA, rootB);
+
+
+//    public WritableImage randomColorImg(ActionEvent actionEvent) {
+//
+//        Image image = blackAndWhiteImageView.getImage(); //get the chosen image from the black image view
+//        PixelReader pixelReader = image.getPixelReader(); //initialize the pixel reader for the original image
+//
+//        width = (int) image.getWidth(); //get width of image
+//        height = (int) image.getHeight(); //get height of image
+//        pixelArray = new int[(height * width)];
+//
+//        if(width <= 0 || height <= 0){
+//            System.out.println("Error in image");
 //        }
+//        pixelArray = new int[(height * width)];
+//        Arrays.fill(pixelArray, -1); // initialize all pixels to -1
+//
+//        WritableImage randomColorImage = new WritableImage(width, height); //create a new WritableImage object with the same dimensions as the original image
+//        PixelWriter pixelWriter = randomColorImage.getPixelWriter();
+//
+//        //loop through each pixel of the original image
+//        for (int row = 0; row < height; row++) {
+//            for (int column = 0; column < width; column++) {
+//
+//                //get the red, green, and blue values of the current pixel
+//                double red = pixelReader.getColor(column, row).getRed();
+//                double green = pixelReader.getColor(column, row).getGreen();
+//                double blue = pixelReader.getColor(column, row).getBlue();
+//
+//                //calculate the luminance of the current pixel
+//                double luminance = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
+//                double luminanceThreshold = Double.parseDouble(thresh.getText());
+//
+//                //determine whether the pixel should be coloured or black based on the luminance threshold
+//                if (luminance >= luminanceThreshold) {
+//                    //generate a random color for the current pixel
+//                    Color randomColor = Color.rgb((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+//                    randomColorImage.getPixelWriter().setColor(column, row, randomColor);
+//                } else {
+//                    randomColorImage.getPixelWriter().setColor(column, row, BLACK);
+//                }
+//
+//            }
+//        }
+//        blackAndWhiteImageView.setImage(randomColorImage); //set the grayImageView to display the new grayscale image
+//        return randomColorImage; //return the new colored image
 //    }
 
+    public void randomColorImg(ActionEvent actionEvent) {
+        Image blackAndWhiteImage = blackAndWhiteImageView.getImage(); // get black and white image
+        PixelReader pixelReader = blackAndWhiteImage.getPixelReader(); // initialize pixel reader
 
-    public WritableImage randomColorImg(ActionEvent actionEvent) {
-
-        Image image = blackAndWhiteImageView.getImage(); //get the chosen image from the black image view
-        PixelReader pixelReader = image.getPixelReader(); //initialize the pixel reader for the original image
-
-        width = (int) image.getWidth(); //get width of image
-        height = (int) image.getHeight(); //get height of image
-        pixelArray = new int[(height * width)];
-
-        if(width <= 0 || height <= 0){
-            System.out.println("Error in image");
-        }
-        pixelArray = new int[(height * width)];
-        Arrays.fill(pixelArray, -1); // initialize all pixels to -1
-
-        WritableImage randomColorImage = new WritableImage(width, height); //create a new WritableImage object with the same dimensions as the original image
+        WritableImage randomColorImage = new WritableImage(width, height); // create a new WritableImage object with the same dimensions as the original image
         PixelWriter pixelWriter = randomColorImage.getPixelWriter();
 
-        //loop through each pixel of the original image
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < width; column++) {
+        //loops through each pixel of the black and white image
+        for (int row = 0; row < height; row++){
+            for (int column = 0; column < width; column++){
+                int index = (row * width) + column;
+                int root = find(pixelArray, index); //gets the root index of current disjoint set
 
-                //get the red, green, and blue values of the current pixel
-                double red = pixelReader.getColor(column, row).getRed();
-                double green = pixelReader.getColor(column, row).getGreen();
-                double blue = pixelReader.getColor(column, row).getBlue();
-
-                //calculate the luminance of the current pixel
-                double luminance = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
-                double luminanceThreshold = Double.parseDouble(thresh.getText());
-
-                //determine whether the pixel should be coloured or black based on the luminance threshold
-                if (luminance >= luminanceThreshold) {
-                    //generate a random color for the current pixel
-                    Color randomColor = Color.rgb((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
-                    randomColorImage.getPixelWriter().setColor(column, row, randomColor);
-                } else {
-                    randomColorImage.getPixelWriter().setColor(column, row, BLACK);
+                if (!hashMap.containsKey(root)){
+                    pixelWriter.setColor(column, row, BLACK);
                 }
-
             }
         }
-        blackAndWhiteImageView.setImage(randomColorImage); //set the grayImageView to display the new grayscale image
-        return randomColorImage; //return the new colored image
-    }
 
+        for(int row = 0; row < height; row++){
+            for(int column = 0; column < width; column++){
+                int index = (row * width) + column;
+                int root = find(pixelArray, index); //gets the root index of current disjoint set
+
+                if (hashMap.containsKey(root)){
+
+                    //gets a random color for the current disjoint set
+                    int red = (int) (Math.random() * 256);
+                    int green = (int) (Math.random() * 256);
+                    int blue = (int) (Math.random() * 256);
+                    Color color = Color.rgb(red, green, blue);
+
+                    //colours all pixels in the current disjoint set with the same color
+                    for (int pixelIndex : hashMap.get(root)) {
+                        int pixelRow = pixelIndex / width;
+                        int pixelColumn = pixelIndex % width;
+                        pixelWriter.setColor(pixelColumn, pixelRow, color);
+
+
+                    }
+                }
+            }
+            adjustableImage.setImage(randomColorImage); // set the adjustableImage to display the new image
+        }
+    }
 
 
     public void exit(ActionEvent actionEvent){
